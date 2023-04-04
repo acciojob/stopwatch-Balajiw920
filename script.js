62d8ced55ce66f4ec182e448fd9e0394e5d019b4
Balajiw920 <wbalaji2021@gmail.com>
2023-04-05 05:07:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,49 @@
 //your code here
-let time = 0;
-let interval;
-let running = false;
+let startTime, timerInterval;
+const timerEl = document.getElementById("timer");
+const startBtn = document.getElementById("start");
+const pauseBtn = document.getElementById("pause");
+const stopBtn = document.getElementById("stop");
 
-function start() {
-  if (!running) {
-    running = true;
-    interval = setInterval(increment, 10);
-    document.getElementById("start").disabled = true;
-    document.getElementById("pause").disabled = false;
-    document.getElementById("stop").disabled = false;
-  }
+function startTimer() {
+  startTime = Date.now();
+  timerInterval = setInterval(updateTimer, 100);
+  startBtn.disabled = true;
+  pauseBtn.disabled = false;
+  stopBtn.disabled = false;
 }
 
-function pause() {
-  if (running) {
-    running = false;
-    clearInterval(interval);
-    document.getElementById("pause").innerText = "Continue";
-  } else {
-    running = true;
-    interval = setInterval(increment, 10);
-    document.getElementById("pause").innerText = "Pause";
-  }
+function updateTimer() {
+  const elapsedTime = Date.now() - startTime;
+  const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+  const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
+  timerEl.innerText = `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 }
 
-function stop() {
-  running = false;
-  clearInterval(interval);
-  time = 0;
-  document.getElementById("time").innerText = "00:00:00";
-  document.getElementById("start").disabled = false;
-  document.getElementById("pause").disabled = true;
-  document.getElementById("stop").disabled = true;
-  document.getElementById("pause").innerText = "Pause";
+function pauseTimer() {
+  clearInterval(timerInterval);
+  pauseBtn.innerText = "Continue";
+  pauseBtn.onclick = continueTimer;
 }
 
-function increment() {
-  time += 10;
-  let date = new Date(time);
-  let hours = date.getUTCHours().toString().padStart(2, "0");
-  let minutes = date.getUTCMinutes().toString().padStart(2, "0");
-  let seconds = date.getUTCSeconds().toString().padStart(2, "0");
-  let milliseconds = date.getUTCMilliseconds().toString().padStart(3, "0");
-  document.getElementById("time").innerText = `${hours}:${minutes}:${seconds}.${milliseconds}`;
+function continueTimer() {
+  startTime = Date.now() - (Date.now() - startTime);
+  timerInterval = setInterval(updateTimer, 100);
+  pauseBtn.innerText = "Pause";
+  pauseBtn.onclick = pauseTimer;
 }
+
+function stopTimer() {
+  clearInterval(timerInterval);
+  timerEl.innerText = "00:00:00";
+  startBtn.disabled = false;
+  pauseBtn.disabled = true;
+  stopBtn.disabled = true;
+}
+
+startBtn.addEventListener("click", startTimer);
+pauseBtn.addEventListener("click", pauseTimer);
+stopBtn.addEventListener("click", stopTimer);
